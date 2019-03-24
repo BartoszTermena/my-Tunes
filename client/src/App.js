6def17227a0c3d4f09961e55040cd8a21f82220c
bartek <Bartek.NNAAN@wp.pl>
2019-03-24 12:13:23 +0100

    search input with searching songs in api

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ class App extends Component {
   state = {
     songs: [],
     count: 0,
+    search: '',
     offset: 0,
-    per_page: 2
+    per_page: 2,
+    msg: ''
   }
   fetchSongs(){
     axios.get('http://localhost:8080/songs/')
@@ -69,8 +71,38 @@ class App extends Component {
     })
    } else return
    }
+   handleSearch = (e) => {
+      this.setState({
+        search: e.target.value
+      }) 
+   }
+   handleSubmit = () => {
+      console.log(this.state)
+     const { search } = this.state;
+     let link;
+     link = `http://localhost:8080/songs?q=${search}`
+     axios.get(link)
+     .then(res => {
+       if(res.data.data.length > 0) {
+       this.setState({
+         songs: res.data.data,
+         count: res.data.count,
+         msg: ''
+       })
+      } else {
+        this.setState({
+          msg: 'No result'
+        })
+      }
+     })
+     .catch(err => {
+       console.log(err)
+     }) 
+  
+    }
   render() {
-    const { songs, count } = this.state;
+    const { songs, count, msg } = this.state;
+    
     if( count > 0 ) {
       return (
         <div className="App">
@@ -88,7 +120,11 @@ class App extends Component {
           <br />
           <button onClick={() => this.handlePrev()}>prev</button>
           <button onClick={() => this.handleNext()}>next</button>
-
+          <br />
+          <input type="text" placeholder="search" onChange={this.handleSearch}/>
+          <button onClick={this.handleSubmit}>Search</button>
+          <br />
+          {msg.length > 0 ? msg : null}
         </div>
       );
     }
@@ -96,6 +132,7 @@ class App extends Component {
       return (
         <div>
           Loading...
+          
         </div>
       )
     }
